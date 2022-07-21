9b6725ba0e20fc2777201f147e8688ccee579bc7
shamsrza <66381642+shamsrza@users.noreply.github.com>
2022-07-21 22:07:11 +0200

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import "./styles.css";
+import { Navbar, NavbarBrand} from 'reactstrap';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }
+
+export default App;
